84512ff4a4f9df6fd2068cc243129b3974a7040d
LukaTsak <lukatsakadze10@gmail.com>
2025-04-17 13:40:18 +0400

    claswork

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { CardComponent } from "../card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CardComponent],
+  imports: [HeaderComponent, CardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -12,4 +13,13 @@ export class HomeComponent {
  link = 'https://www.ers.ga.gov/sites/main/files/main-images/camera_lense_0.jpeg'
  name = 'name'
  surname = 'surname'
+
+ hide(){
+  this.showLists = !this.showLists
+ }
+
+ showLists = false
+
+ array : string [] = ['aa','bb','cc','dd','ee']
+ arrayy = ['aa','bb']
 }
