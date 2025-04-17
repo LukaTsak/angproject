f59e52a2e28a4da9dfb6acacfd130c2a30aa0c8d
LukaTsak <lukatsakadze10@gmail.com>
2025-04-17 16:37:33 +0400

    studentsAdder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,24 +2,79 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { CardComponent } from "../card/card.component";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CardComponent, CommonModule],
+  imports: [HeaderComponent, CardComponent, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
- link = 'https://www.ers.ga.gov/sites/main/files/main-images/camera_lense_0.jpeg'
- name = 'name'
- surname = 'surname'
+ firstName = ''
+ age = ''
+ grade = ''
+ subjects = ''
+ street = ''
+ city = ''
+ zip = ''
+ check : boolean = false
+ index = 0
 
- hide(){
-  this.showLists = !this.showLists
+ studentsArray : any = []
+
+ studObj = {}
+
+ create(){
+  const studObj = {
+    firstname : this.firstName,
+    Agee : this.age,
+    gradee : this.grade,
+    subj : this.subjects,
+    streett : this.street,
+    cityy : this.city,
+    zipp : this.zip,
+    checkk : this.check,
+    indexx : this.index++
+   }
+
+  this.studentsArray.push (studObj)
+  console.log(this.studentsArray)
+
+  this.firstName = ''
+  this.age = ''
+  this.grade = ''
+  this.subjects = ''
+  this.street = ''
+  this.city = ''
+  this.zip = ''
+  this.check = false
  }
 
- showLists = false
+ delete(index : number){
+  this.studentsArray.splice(index,1)
+ }
 
- array : string [] = ['aa','bb','cc','dd','ee']
- arrayy = ['aa','bb']
+ editt(index : number){
+  this.firstName = this.studentsArray[index].firstname
+  this.age = this.studentsArray[index].agee
+  this.grade = this.studentsArray[index].gradee
+  this.subjects = this.studentsArray[index].subj
+  this.street = this.studentsArray[index].streett
+  this.city = this.studentsArray[index].cityy
+  this.zip = this.studentsArray[index].zipp
+
+  this.delete(index)
+ }
+
+ checkDel(){
+  console.log(this.studentsArray)
+  for(let el of this.studentsArray){
+    if(el.checkk == true){
+      this.studentsArray.splice(el.indexx,1)
+    }
+  }
+ }
+
 }
