fb6bb41b18404ad2e5e45795e907cf6b42a85c47
LukaTsak <lukatsakadze10@gmail.com>
2025-04-24 22:37:19 +0400

    routingAndLinks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+
+    { path: "details/:id", component: DetailsComponent },
+    { path: 'home', component: HomeComponent },
+
+    {path: '**', redirectTo: ''}
+];
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  cardId ?: number;
+
+  constructor(private routee: ActivatedRoute) { 
+    this.routee.params.subscribe(paramss => {
+      this.cardId = paramss['id'];
+      console.log(this.cardId);
+      // this.firstName = this.studentsArray[this.index].first_name;
+      // this.age = this.studentsArray[this.index].last_name;
+      // this.grade = this.studentsArray[this.index].email;
+      // this.subjects = this.studentsArray[this.index].avatar;
+    });
+  }
+
+
+
+
+
   @Input() el: any;
   @Input() index?: number;
 
